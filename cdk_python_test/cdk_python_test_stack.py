b64085d1534044d1f77b516cf06d990675ffd8f5
corymhall <43035978+corymhall@users.noreply.github.com>
2022-02-17 19:06:36 +0000

    more commits

--- cdk_python_test/cdk_python_test_stack.py
+++ cdk_python_test/cdk_python_test_stack.py
@@ -1,8 +1,12 @@
+import os
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
+    aws_lambda as _lambda,
     # aws_sqs as sqs,
 )
+
+import aws_cdk.aws_lambda_python_alpha as lambda_python
 from constructs import Construct
 
 class CdkPythonTestStack(Stack):
@@ -12,8 +16,17 @@ class CdkPythonTestStack(Stack):
 
         # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkPythonTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        lambda_python.PythonFunction(
+            self, "handlingFunction",
+            entry=os.path.join(os.path.dirname(
+                os.path.realpath(__file__)), "../src/lambda"),
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            index="lambda_function.py",
+            environment={
+                "LOGLEVEL": "DEBUG"
+            },
+            timeout=Duration.seconds(20),
+            memory_size=512,
+            tracing=_lambda.Tracing.ACTIVE
+        )
+
